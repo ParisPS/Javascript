3ae3f4d42528275487f9814d17d07f58bb94f222
ParisPS <paris.perez.s@gmail.com>
2021-03-29 00:25:55 -0300

    commit

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,57 @@
+function clicked(){
+    document.getElementById("thank").innerHTML = "<b>Thanks for clicking</b>";
+
+    //console.log(document.getElementById("thank"))
+    //alert("thanks for clicking");
+}
+
+function redirect(){
+    window.open("https://github.com/ParisPS");
+    //window.location.href = "https://github.com/ParisPS";
+}
+
+function change(elemento){
+    //document.getElementById("mousemove").innerHTML = "Thanks for hovering over here";
+    elemento.innerHTML = "Thanks for hovering over here";
+    //alert("change text");
+}
+
+function turn(elemento){
+    //document.getElementById("mousemove").innerHTML = "Mouse over here";
+    elemento.innerHTML = "Mouse over here";
+}
+
+function load(){
+    alert("page loaded");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/* function validaIdade (idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual a sua idade?");
+console.log(validaIdade(idade));
+
+/* function soma(n1, n2) {
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace ("Anime", "Star Wars", "Marvel"));
+alert(soma(5, 10)) */
+
 /* var d = new Date();
 alert(d.getMonth()+1) */
 

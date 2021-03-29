3286b17e61af301ee406ef5a09dbb93f547b2f34
ParisPS <paris.perez.s@gmail.com>
2021-03-28 21:20:06 -0300

    commit

--- Js/main.js
+++ Js/main.js
@@ -1,8 +1,30 @@
 
+
+/* var count;
+for (count=0; count <= 5; count++){
+       alert(count);
+}; */
+
+/* var idade = prompt("Qual a sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}; */
+
+/* var frutas = [{name: "maçã", cor:"vermelha"}, {name:"uva", cor:"roxa"}]
+console.log(frutas);
+
+
 var name = "Paris PS";
 var age = 17;
 var age2 = 10;
-// alert(name + " tem " + age + " anos");
-// alert (age + age2);
+var list = ["Maçã", " laranja", " limão"];
+list.push(" uva")
+
+alert(name + " tem " + age + " anos");
+
 console.log(name);
 console.log(age + age2);
+console.log(list[0])
+console.log(list.join(" - ")); */
